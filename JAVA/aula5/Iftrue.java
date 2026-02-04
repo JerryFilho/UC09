e5a7f94c0b10dece937f0dca5c600619fc1e76b4
JerryFilho <jerryfilho123@gmail.com>
2026-02-04 20:01:53 -0300

    123

---
+++ JAVA/aula5/Iftrue.java
@@ -0,0 +1,21 @@
+package aula5;
+
+public class Iftrue {
+    public static void main(String[] args) {
+        
+        //declaração de variavel
+        int x = 0;
+        int y = 0;
+
+        if (x == 0) {
+            if (y == 0) {
+                System.out.println("o que podemos concluir de X e Y?");
+                
+            }
+        }
+
+        if (x == 0 && y == 0) {
+            System.out.println("");
+        }
+    }
+}
