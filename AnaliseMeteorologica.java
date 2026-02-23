a24f64c4a691d013e2d3c3613c54b43f3620ebd6
JerryFilho <jerryfilho123@gmail.com>
2026-02-23 19:15:20 -0300

    sei menos ainda

---
+++ AnaliseMeteorologica.java
@@ -0,0 +1,9 @@
+// Criar um sistema em Java que analisa dados meteorológicos de 
+// múltiplas cidades, identifica padrões, gera alertas e 
+// calcula estatísticas complexas. 
+
+
+public class AnaliseMeteorologica {
+
+    
+}
---
+++ JAVA/aula6/Strings.java
@@ -0,0 +1,38 @@
+package aula6;
+
+public class Strings {
+    public static void main(String[] args) {
+        String fruta = "banAna";
+        
+        String s1 ="Alan turing";
+        String s2 = "Ada Lovelace";
+        char letra;
+
+        letra = 'a';
+        for (int i = 0; i < 95; i++);
+        System.out.println((char)(letra + 1 ));
+
+        // int diff = s1.compareTo(s2);
+        
+        // if (diff < 0 ){
+        //     System.out.println("s1 vem antes do s2");
+        // }
+        // else if (diff > 0){
+        //     System.out.println("s2 vem antes do s1");
+        // }
+        // else {
+        //     System.out.println("os nomes são iguais");
+        // }
+
+        //char letra;
+        //System.out.println(fruta.substring(5,6));
+        // for (char c = 'a'; c <= 'z' ; c++){
+        //     System.out.printf("%d ", (int)c);
+        // }
+        // for (int i= 0; i <= fruta.length(); i++){
+        //     letra = fruta.charAt(1);
+        //     System.out.println();
+        // }
+
+    }
+}
